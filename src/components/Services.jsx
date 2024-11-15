2b508f5f4dc44ab9b7fc721d839ee1a39c014937
SANJOY <paulsanjoy2923@gmail.com>
2024-11-15 10:47:44 +0530

    services added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,37 +4,37 @@ function Services() {
             <h1 className="text-5xl font-bold">Our Services</h1>
             <div className="flex flex-col gap-5">
                 <div className="flex text-center p-2 items-center justify-between gap-5">
-                    <div className="w-[30vw] text-center flex flex-col justify-center border-black border-2">
-                        <i class="fa-solid fa-camera"></i>
-                        <h2>Photo Editing</h2>
-                        <p>Transform your photos with our professional editing services.</p>
+                    <div className="w-[30vw] text-center flex flex-col justify-center p-4 gap-2">
+                        <i class="fa-solid fa-camera text-4xl"></i>
+                        <h2 className="font-bold text-xl">Photo Editing</h2>
+                        <p className="text-sm text-black/60 font-normal">Transform your photos with our professional editing services.</p>
                     </div>
-                    <div className="w-[30vw] text-center flex flex-col justify-center border-black border-2">
-                        <i class="fa-solid fa-video"></i>
-                        <h2>Video Editing</h2>
-                        <p>Create stunning videos with our expert editing techniques.</p>
+                    <div className="w-[30vw] text-center flex flex-col justify-center p-4 gap-2">
+                        <i class="fa-solid fa-video text-4xl"></i>
+                        <h2 className="font-bold text-xl">Video Editing</h2>
+                        <p className="text-sm text-black/60 font-normal">Create stunning videos with our expert editing techniques.</p>
                     </div>
-                    <div className="w-[30vw] text-center flex flex-col justify-center border-black border-2">
-                        <i class="fa-solid fa-pen-nib"></i>
-                        <h2>Graphics Designing</h2>
-                        <p>Bring your ideas to life with our creative graphic design solutions.</p>
+                    <div className="w-[30vw] text-center flex flex-col justify-center p-4 gap-2">
+                        <i class="fa-solid fa-pen-nib text-4xl"></i>
+                        <h2 className="font-bold text-xl">Graphics Designing</h2>
+                        <p className="text-sm text-black/60 font-normal">Bring your ideas to life with our creative graphic design solutions.</p>
                     </div>
                 </div>
                 <div className="flex text-center p-2 items-center justify-between gap-5">
-                    <div className="w-[30vw] text-center flex flex-col justify-center border-black border-2">
-                        <i class="fa-solid fa-paintbrush"></i>
-                        <h2>Poster Making</h2>
-                        <p>Create eye-catching posters that grab attention and convey your message.</p>
+                    <div className="w-[30vw] text-center flex flex-col justify-center p-4 gap-2">
+                        <i class="fa-solid fa-paintbrush text-4xl"></i>
+                        <h2 className="font-bold text-xl">Poster Making</h2>
+                        <p className="text-sm text-black/60 font-normal">Create eye-catching posters that grab attention and convey your message.</p>
                     </div>
-                    <div className="w-[30vw] text-center flex flex-col justify-center border-black border-2">
-                        <i class="fa-solid fa-code"></i>
-                        <h2>Website Making</h2>
-                        <p>Build responsive and functional websites tailored to your needs.</p>
+                    <div className="w-[30vw] text-center flex flex-col justify-center px-4 h-[19vh] gap-2 mb-5">
+                        <i class="fa-solid fa-code text-4xl"></i>
+                        <h2 className="font-bold text-xl">Website Making</h2>
+                        <p className="text-sm text-black/60 font-normal">Build responsive and functional websites tailored to your needs.</p>
                     </div>
-                    <div className="w-[30vw] text-center flex flex-col justify-center border-black border-2">
-                        <i class="fa-solid fa-palette"></i>
-                        <h2>Web Designing</h2>
-                        <p>Create visually appealing and user-friendly web designs.</p>
+                    <div className="w-[30vw] text-center flex flex-col justify-center px-4 h-[19vh] gap-2 mb-5">
+                        <i class="fa-solid fa-palette text-4xl"></i>
+                        <h2 className="font-bold text-xl">Web Designing</h2>
+                        <p className="text-sm text-black/60 font-normal">Create visually appealing and user-friendly web designs.</p>
                     </div>
                 </div>
             </div>
