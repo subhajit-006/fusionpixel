af6d22565ea2593ca0d663a39174d29dcd8d9045
SANJOY <paulsanjoy2923@gmail.com>
2024-11-19 08:39:54 +0530

    mailto and tel added

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -10,9 +10,8 @@ function Navbar(){
             <ul className="flex gap-8 text-lg font-medium">
                 <li>Home</li>
                 <li>Services <i className="fa-solid fa-caret-down"></i></li>
-                <li>Orders</li>
                 <li>Contact Us</li>
-                <li>Your Account</li>
+                <li>About Us</li>
             </ul>
         </div>
     )
