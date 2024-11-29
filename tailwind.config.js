6eaed58d9c2ef760838fd2efa04024f6df809cd0
SANJOY <paulsanjoy2923@gmail.com>
2024-11-29 10:55:10 +0530

    new fonts addedc

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fontNeue: ["Bebas Neue"],
+        fontRal: ["Raleway"],
+      }
+    },
   },
   plugins: [],
 }
