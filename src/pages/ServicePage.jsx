fbada06fbad18215eb6a675a24658e2911c20c0a
SANJOY <paulsanjoy2923@gmail.com>
2024-12-13 20:55:31 +0530

    cursor changed

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -13,8 +13,8 @@ function ServicePage() {
     return (
         <>
             <Cursor />
-            <Nabvar />
-            <div className="h-[100h] flex flex-col justify-center gap-10 py-16">
+            <div className="h-[100h] flex flex-col justify-center gap-10 pb-16 bg-[#DFF2EB]">
+                <Nabvar />
                 <div>
                     <h1 className="text-5xl text-center font-bold font-fontProtest">Our Services</h1>
                 </div>
@@ -22,7 +22,7 @@ function ServicePage() {
                     <div className="flex text-center p-2 items-center justify-evenly gap-5">
 
                         <Link to="/PhotoShootPage">
-                            <div className="relative w-[25vw] h-[35vh] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
+                            <div className="relative w-[25vw] h-[35vh] bg-[#ecf9f4] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
                                 {/* Background Video */}
                                 <video
                                     className="absolute top-0 left-0 w-full h-full opacity-0 object-cover -z-10 transition-opacity duration-300 group-hover:opacity-100"
@@ -36,7 +36,7 @@ function ServicePage() {
                                 {/* Card Content */}
                                 <div className="relative z-10">
                                     <i className="fa-solid fa-camera text-4xl"></i>
-                                    <h2 className="font-bold text-xl">Photo Shoot</h2>
+                                    <h2 className="text-xl font-thin font-fontDyan">Photo Shoot</h2>
                                     <p className="text-sm font-normal">
                                         Capture your moments with our professional photo shoot services.
                                     </p>
@@ -45,7 +45,7 @@ function ServicePage() {
                         </Link>
 
                         <Link to="/VideoShootPage">
-                            <div className="relative w-[25vw] h-[35vh] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
+                            <div className="relative w-[25vw] h-[35vh] bg-[#ecf9f4] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
                                 {/* Background Video */}
                                 <video
                                     className="absolute top-0 left-0 w-full h-full opacity-0 object-cover -z-10 transition-opacity duration-300 group-hover:opacity-100"
@@ -59,7 +59,7 @@ function ServicePage() {
                                 {/* Card Content */}
                                 <div className="relative z-10">
                                     <i className="fa-solid fa-video text-4xl"></i>
-                                    <h2 className="font-bold text-xl">Video Shoot</h2>
+                                    <h2 className="text-xl font-thin font-fontDyan">Video Shoot</h2>
                                     <p className="text-sm font-normal">
                                         Capture your moments with our professional video shoot services.
                                     </p>
@@ -68,7 +68,7 @@ function ServicePage() {
                         </Link>
 
                         <Link to="/PhotoAndVideoEditingPage">
-                            <div className="relative w-[25vw] h-[35vh] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
+                            <div className="relative w-[25vw] h-[35vh] bg-[#ecf9f4] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
                                 {/* Background Video */}
                                 <video
                                     className="absolute top-0 left-0 w-full h-full opacity-0 object-cover -z-10 transition-opacity duration-300 group-hover:opacity-100"
@@ -82,7 +82,7 @@ function ServicePage() {
                                 {/* Card Content */}
                                 <div className="relative z-10">
                                     <i className="fa-solid fa-paintbrush text-4xl"></i>
-                                    <h2 className="font-bold text-xl">Photo & Video Editing</h2>
+                                    <h2 className="text-xl font-thin font-fontDyan">Photo & Video Editing</h2>
                                     <p className="text-sm font-normal">
                                         Transform your photos and videos with our professional editing
                                         services.
@@ -94,7 +94,7 @@ function ServicePage() {
 
                     <div className="flex text-center p-2 items-center justify-evenly gap-5">
                         <Link to="/GraphicsDesigningPage">
-                            <div className="relative w-[25vw] h-[35vh] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
+                            <div className="relative w-[25vw] h-[35vh] bg-[#ecf9f4] hover:text-white text-center flex flex-col justify-center p-4 gap-2 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
                                 {/* Background Video */}
                                 <video
                                     className="absolute top-0 left-0 w-full h-full opacity-0 object-cover -z-10 transition-opacity duration-300 group-hover:opacity-100"
@@ -108,7 +108,7 @@ function ServicePage() {
                                 {/* Card Content */}
                                 <div className="relative z-10">
                                     <i className="fa-solid fa-pen-nib text-4xl"></i>
-                                    <h2 className="font-bold text-xl">Graphics Designing</h2>
+                                    <h2 className="text-xl font-thin font-fontDyan">Graphics Designing</h2>
                                     <p className="text-sm font-normal">
                                         Bring your ideas to life with our creative graphic design solutions.
                                     </p>
@@ -117,7 +117,7 @@ function ServicePage() {
                         </Link>
 
                         <Link to="/WebDevPage">
-                            <div className="relative w-[25vw] h-[35vh] hover:text-white text-center flex flex-col justify-center px-4 gap-2 mb-5 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
+                            <div className="relative w-[25vw] h-[35vh] bg-[#ecf9f4] hover:text-white text-center flex flex-col justify-center px-4 gap-2 mb-5 border-2 rounded-lg border-black/35 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] transition-transform duration-300 hover:scale-105 overflow-hidden group">
                                 {/* Background Video */}
                                 <video
                                     className="absolute top-0 left-0 w-full h-full opacity-0 object-cover -z-10 transition-opacity duration-300 group-hover:opacity-100"
@@ -131,7 +131,7 @@ function ServicePage() {
                                 {/* Card Content */}
                                 <div className="relative z-10">
                                     <i className="fa-solid fa-code text-4xl"></i>
-                                    <h2 className="font-bold text-xl">Website Making</h2>
+                                    <h2 className="text-xl font-thin font-fontDyan">Website Making</h2>
                                     <p className="text-sm font-normal">
                                         Build responsive and functional websites tailored to your needs.
                                     </p>
@@ -140,7 +140,7 @@ function ServicePage() {
                         </Link>
 
                         <Link to="/WebDesigningPage" aria-label="Navigate to Web Designing Page">
-                            <div className="relative w-[25vw] h-[35vh] hover:text-white border-2 rounded-lg border-black/35 text-center flex flex-col justify-center px-4 gap-2 mb-5 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] overflow-hidden group transition-transform duration-300 hover:scale-105">
+                            <div className="relative w-[25vw] h-[35vh] bg-[#ecf9f4] hover:text-white border-2 rounded-lg border-black/35 text-center flex flex-col justify-center px-4 gap-2 mb-5 hover:shadow-[18px_18px_15px_-7px_#636363] hover:border-[#636363] overflow-hidden group transition-transform duration-300 hover:scale-105">
                                 {/* Background Video */}
                                 <video
                                     className="absolute top-0 left-0 w-full h-full opacity-0 object-cover -z-10 transition-opacity duration-300 group-hover:opacity-100"
@@ -154,7 +154,7 @@ function ServicePage() {
                                 {/* Card Content */}
                                 <div className="relative z-10">
                                     <i className="fa-solid fa-palette text-4xl"></i>
-                                    <h2 className="font-bold text-xl">Web Designing</h2>
+                                    <h2 className="text-xl font-thin font-fontDyan">Web Designing</h2>
                                     <p className="text-sm font-normal">
                                         Create visually appealing and user-friendly web designs.
                                     </p>
