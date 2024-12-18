70393c8e5a8fe314b1b91381c6652f19df57c956
SANJOY <paulsanjoy2923@gmail.com>
2024-12-18 08:03:31 +0530

    vercel.json added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,17 +8,6 @@ function Contact() {
                 Join FushionPixelLabs today and let us help you create stunning digital content.
             </p>
             <div className="flex flex-col gap-3 w-full sm:w-auto">
-                <div className="flex flex-col sm:flex-row gap-3 sm:gap-5 items-center">
-                    <input 
-                        name="" 
-                        id="" 
-                        placeholder="Enter Your Email" 
-                        className="p-2 border border-black/20 rounded w-full sm:w-64"
-                    />
-                    <button className="border-2 border-black p-2 font-semibold bg-black text-white rounded hover:bg-black/80 hover:border-black/80 w-full sm:w-auto">
-                        Get Started
-                    </button>
-                </div>
                 <p className="text-xs text-center text-black/60 font-fontDyan font-thin">
                     By subscribing, you agree to our Terms of Service and Privacy Policy.
                 </p>
